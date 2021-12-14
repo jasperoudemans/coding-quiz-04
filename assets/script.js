7098a310b140931141f93c3a6a17a7cf14d326c6
Jasper Oudemans <jasperoudemans@gmail.com>
2021-12-13 19:24:21 -0700

    fixed timer bug and styled button

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,21 @@
-
 var time = document.getElementById("time");
 var seconds = 10;
 var startButton = document.getElementById("startBtn");
 
 function startTime() {
-    var interval = setInterval(function() {
-        seconds--;
-        time.textContent = seconds;
-        
-        if (seconds === 0) {
-            clearInterval(interval);
-            stopGame();
-        }
-    }, 1000);
+  var interval = setInterval(function () {
+    if (seconds == 0) {
+      clearInterval(interval);
+    } else {
+      seconds--;
+      time.textContent = seconds;
+
+      if (seconds === 0) {
+        clearInterval(interval);
+        stopGame();
+      }
+    }
+  }, 1000);
 }
 
 startButton.addEventListener("click", startTime);
